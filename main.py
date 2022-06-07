813fc3ef4f83e360aa372304b8e1d269280500ec
ahamedaslam <87639294+ahamedaslam@users.noreply.github.com>
2022-06-07 10:01:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#creation of signup system:
+print('Create your Account!!!!!')
+username = input("Enter username: ")
+password = input("Enter password: ")
+print('Account created successfully!!')
+
+#creation of login system:
+print("Now login into your account!!")
+username1 = input("Enter username: ")
+password1 = input("Enter password: ")
+
+if username == username1 and password == password1:
+    print('Login successfully!!!!')
+else:
+    print(" woops!!Invalid Credentials")
+
+
+
+
+
+
